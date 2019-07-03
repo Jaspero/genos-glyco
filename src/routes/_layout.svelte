0e0035396e11c993ee0c217a4f3a6ae296cc6ffc
svendjanis <sven.djanis@gmail.com>
2019-07-03 14:58:15 +0200

    feat: working on progress bar #10

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,14 +1,21 @@
 <script>
-	import * as sapper from '@sapper/app';
+  /*  import {beforeUpdate, afterUpdate} from 'svelte'
+    import * as sapper from '@sapper/app';*/
 	export let segment;
 
 	   function toggleMenu() {
           const menu = document.querySelector('.gg-mobile-menu');
           menu.classList.toggle('active')
         }
+/*
+          beforeUpdate(() => {
+              console.log('destroy')
+            })
+
+            afterUpdate(() => {
+                console.log(321)
+            })*/
 </script>
-
-
 
 <style>
 .gg-header {
