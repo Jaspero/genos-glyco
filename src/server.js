3f14ef63ead13a88717e8f3d67f379ccb2c94716
svendjanis <sven.djanis@gmail.com>
2019-07-22 13:28:29 +0200

    initial commit

--- src/server.js
+++ src/server.js
@@ -4,16 +4,9 @@ import compression from 'compression';
 import admin from 'firebase-admin';
 import * as sapper from '@sapper/server';
 import {json} from 'body-parser';
+import {ENV_CONFIG} from '../env-config';
 
-admin.initializeApp({
-	apiKey: 'AIzaSyBEWKn6Dw8cvgUBwbmDkM9VKHKEf0Q5svc',
-	authDomain: 'genos-glyco.firebaseapp.com',
-	databaseURL: 'https://genos-glyco.firebaseio.com',
-	projectId: 'genos-glyco',
-	storageBucket: 'genos-glyco.appspot.com',
-	messagingSenderId: '147855481401',
-	appId: '1:147855481401:web:b7a554fc44bae97f'
-});
+admin.initializeApp(ENV_CONFIG.firebase);
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
