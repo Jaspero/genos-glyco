5e76393d067710fa52b5ef59e53ad2b51230d45e
Filip Lauc <filip.lauc93@gmail.com>
2022-01-27 10:37:55 +0100

    chore: updated page

--- src/routes/data-down.svelte
+++ src/routes/data-down.svelte
@@ -1,3 +1,25 @@
-<p>Biological age in people with Down syndrome is on average 19 years older, not due to co-morbidities, but implicating a 31-gene region</p>
+<svelte:head>
+  <title>Down Data | Genos Glyco</title>
+</svelte:head>
 
-<a href="/assets/data.xls">Data</a>
+<!--Services intro-->
+<section class="gg-services-intro">
+  <div class="grid">
+    <div class="col-12">
+      <h4 class="gg-title c-l-primary">
+        <p>Biological age in people with Down syndrome is on average 19 years older, not due to co-morbidities, but implicating a 31-gene region</p>
+      </h4>
+      <a href="/assets/data.xls">Data</a>
+    </div>
+  </div>
+</section>
+
+<style>
+  .gg-services-intro {
+    padding: 200px 0 120px;
+    background-image: url("/assets/images/section-intro.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+  }
+</style>
